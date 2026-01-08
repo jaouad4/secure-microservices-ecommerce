93646a1dfa3ffe420865296216f128987e279412
jaouad4 <salahjd16@gmail.com>
2026-01-08 18:36:15 +0100

    Initial DevSecOps configuration

---
+++ src/test/java/ma/enset/securemicroservicesecommerce/SecureMicroservicesEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.securemicroservicesecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMicroservicesEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
