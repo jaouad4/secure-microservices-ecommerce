73b6ab0acd24f61d53c00a2aba65eea976e87e0e
DJILI Mohamed Amine <aminedjili29@gmail.com>
2026-01-10 13:53:14 +0100

    move microservices into backend folder

---
+++ backend/discovery-service/src/test/java/ma/sieger/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sieger.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
