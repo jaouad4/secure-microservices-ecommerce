e7346e94f1a9e0e9cc00af9f33ad25f7a03c400d
DJILI Mohamed Amine <aminedjili29@gmail.com>
2026-01-10 10:45:36 +0100

    Adding Eureka Discovery Service

--- discovery-service/src/test/java/ma/sieger/discoveryservice/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/ma/sieger/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package ma.enset.securemicroservicesecommerce;
+package ma.sieger.discoveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecureMicroservicesEcommerceApplicationTests {
+class DiscoveryServiceApplicationTests {
 
     @Test
     void contextLoads() {
