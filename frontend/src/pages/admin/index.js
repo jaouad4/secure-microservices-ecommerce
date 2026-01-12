dbbc56cc97077d54dc1f921875c6549fb62e92b6
jaouad4 <salahjd16@gmail.com>
2026-01-12 02:48:04 +0100

    Fix bugs

--- frontend/src/pages/admin/index.js
+++
@@ -1,4 +0,0 @@
-// Admin pages exports
-export { default as DashboardPage } from './DashboardPage'
-export { default as ProductManagement } from './ProductManagement'
-export { default as OrderManagement } from './OrderManagement'
--- frontend/src/pages/client/index.js
+++
@@ -1,6 +0,0 @@
-// Client pages exports
-export { default as CatalogPage } from './CatalogPage'
-export { default as ProductDetailPage } from './ProductDetailPage'
-export { default as CartPage } from './CartPage'
-export { default as CheckoutPage } from './CheckoutPage'
-export { default as MyOrdersPage } from './MyOrdersPage'
